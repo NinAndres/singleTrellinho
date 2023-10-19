330916540fd0f2da85eddc38e6f50e2ddd1ded0f
JLisarte <juliano.lisarte@gmail.com>
2023-10-18 21:28:25 -0300

    feat - botão de fechar

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const todoTasks = document.getElementById("todo-tasks");
-  const inProgressTasks = document.getElementById("in-progress-tasks");
-  const doneTasks = document.getElementById("done-tasks");
+  const todoTasks = document.getElementById("todo-tasks")
+  const inProgressTasks = document.getElementById("in-progress-tasks")
+  const doneTasks = document.getElementById("done-tasks")
 
-  const addTaskForm = document.getElementById("add-task-form");
+  const addTaskForm = document.getElementById("add-task-form")
 
-  const tasks = JSON.parse(localStorage.getItem("TrellinhoTask")) || [];
+  const tasks = JSON.parse(localStorage.getItem("TrellinhoTask")) || []
 
-  renderTasks();
+  renderTasks()
 
   addTaskForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const taskText = document.getElementById("task-text").value;
-    const taskDescription = document.getElementById("task-description").value;
+    e.preventDefault()
+    const taskText = document.getElementById("task-text").value
+    const taskDescription = document.getElementById("task-description").value
 
     if (taskText && taskDescription) {
       const newTask = {
@@ -20,138 +20,140 @@ document.addEventListener("DOMContentLoaded", () => {
         text: taskText,
         description: taskDescription,
         status: "todo",
-      };
-      tasks.push(newTask);
-      saveTasks();
-      renderTasks();
-      addTaskForm.reset();
+      }
+      tasks.push(newTask)
+      saveTasks()
+      renderTasks()
+      addTaskForm.reset()
     }
-  });
+  })
 
   function renderTasks() {
-    todoTasks.innerHTML = "";
-    inProgressTasks.innerHTML = "";
-    doneTasks.innerHTML = "";
+    todoTasks.innerHTML = ""
+    inProgressTasks.innerHTML = ""
+    doneTasks.innerHTML = ""
 
     tasks.forEach((task) => {
-      const taskElement = createTaskElement(task);
-      appendTaskToColumn(taskElement, task.status);
-    });
+      const taskElement = createTaskElement(task)
+      appendTaskToColumn(taskElement, task.status)
+    })
 
-    addDragAndDropListeners();
+    addDragAndDropListeners()
   }
 
   function createTaskElement(task) {
-    const taskElement = document.createElement("div");
-    taskElement.classList.add("task");
-    taskElement.draggable = true;
-    taskElement.dataset.taskId = task.id;
+    const taskElement = document.createElement("div")
+    taskElement.classList.add("task")
+    taskElement.draggable = true
+    taskElement.dataset.taskId = task.id
 
     taskElement.innerHTML = `
     <div class="task-container">
+    <div class="removeButton">
+      <i class="remove-task-button" data-task-id="${task.id}">X</i>
+    </div>
     <div>
-       <strong>${task.text}</strong>
+      <strong>${task.text}</strong>
     </div>
     <div>${task.description}</div>
-    <button class="remove-task-button" data-task-id="${task.id}">Remover</button>
     </div>
-    `;
+    `
 
     taskElement
       .querySelector(".remove-task-button")
       .addEventListener("click", (e) => {
-        const taskId = e.target.dataset.taskId;
-        removeTask(taskId);
-      });
+        const taskId = e.target.dataset.taskId
+        removeTask(taskId)
+      })
 
     taskElement.addEventListener("dragstart", (e) => {
-      e.dataTransfer.setData("text/plain", task.id);
-    });
+      e.dataTransfer.setData("text/plain", task.id)
+    })
 
-    return taskElement;
+    return taskElement
   }
 
   function appendTaskToColumn(taskElement, status) {
     switch (status) {
       case "todo":
-        todoTasks.appendChild(taskElement);
-        break;
+        todoTasks.appendChild(taskElement)
+        break
       case "in-progress":
-        inProgressTasks.appendChild(taskElement);
-        break;
+        inProgressTasks.appendChild(taskElement)
+        break
       case "done":
-        doneTasks.appendChild(taskElement);
-        break;
+        doneTasks.appendChild(taskElement)
+        break
       default:
-        console.error("Invalid status");
+        console.error("Invalid status")
     }
   }
 
   function saveTasks() {
-    localStorage.setItem("TrellinhoTask", JSON.stringify(tasks));
+    localStorage.setItem("TrellinhoTask", JSON.stringify(tasks))
   }
 
   function generateTaskId() {
-    return "_" + Math.random().toString(36).substr(2, 9);
+    return "_" + Math.random().toString(36).substr(2, 9)
   }
 
   function addDragAndDropListeners() {
-    const columns = document.querySelectorAll(".tasks");
+    const columns = document.querySelectorAll(".tasks")
 
     columns.forEach((column) => {
-      column.addEventListener("dragover", allowDrop);
-      column.addEventListener("drop", drop);
-    });
+      column.addEventListener("dragover", allowDrop)
+      column.addEventListener("drop", drop)
+    })
   }
 
   function allowDrop(event) {
-    event.preventDefault();
+    event.preventDefault()
   }
 
   function drop(event) {
-    event.preventDefault();
-    const draggedTaskId = event.dataTransfer.getData("text/plain");
+    event.preventDefault()
+    const draggedTaskId = event.dataTransfer.getData("text/plain")
     const draggedTaskElement = document.querySelector(
       `[data-task-id="${draggedTaskId}"]`
-    );
+    )
 
     if (draggedTaskElement) {
-      const targetColumn = event.target.closest(".tasks");
-      targetColumn.appendChild(draggedTaskElement);
+      const targetColumn = event.target.closest(".tasks")
+      targetColumn.appendChild(draggedTaskElement)
 
-      TaskStatus(draggedTaskId, targetColumn.id);
+      TaskStatus(draggedTaskId, targetColumn.id)
     }
   }
 
   function TaskStatus(taskId, columnId) {
-    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+    const taskIndex = tasks.findIndex((task) => task.id === taskId)
 
     if (taskIndex !== -1) {
       switch (columnId) {
         case "todo-tasks":
-          tasks[taskIndex].status = "todo";
-          break;
+          tasks[taskIndex].status = "todo"
+          break
         case "in-progress-tasks":
-          tasks[taskIndex].status = "in-progress";
-          break;
+          tasks[taskIndex].status = "in-progress"
+          break
         case "done-tasks":
-          tasks[taskIndex].status = "done";
-          break;
+          tasks[taskIndex].status = "done"
+          break
         default:
-          console.error("Invalid column ID");
+          console.error("Invalid column ID")
       }
 
-      saveTasks();
+      saveTasks()
     }
   }
 
   function removeTask(taskId) {
-    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+    const taskIndex = tasks.findIndex((task) => task.id === taskId)
 
     if (taskIndex !== -1) {
-      tasks.splice(taskIndex, 1);
-      saveTasks();
-      renderTasks();
+      tasks.splice(taskIndex, 1)
+      saveTasks()
+      renderTasks()
     }
   }
-});
+})
